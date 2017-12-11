092e86d5588958c0581140f80b0abe8e0dd62ec6
Crista Cooper <aine3d@gmail.com>
2017-12-11 16:18:45 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,10 +11,13 @@ class String
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.end_with?("!") || self.end_with?(".") || self.end_with?("?")
+      self.split
+      self.count
+    else return false
   end
 end
